29e01d2f46eab80fa79b7cbc6ad77de919c5f6a0
Ross Macduff <ross.macduff@mercatus.com>
2017-03-29 05:59:44 -0400

    add setup.py and setup.cfg so this can be built as a package

---
+++ setup.py
@@ -0,0 +1,50 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='awssearch',
+
+    version='0.0.1',
+
+    description='Search AWS inventory across accounts and regions',
+    long_description=long_description,
+
+    url='https://gitlab.com/rmacduff/aws-search',
+
+    author='Ross Macduff',
+    author_email='ross@macduff.ca',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: System Administrators',
+        'Topic :: Infrastructure Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='aws infrastructure',
+
+    install_requires=['terminaltables', 'boto3', 'pyyaml'],
+
+    entry_points={
+        'console_scripts': [
+            'awssearch = awssearch.cli:main',
+        ],
+    },
+    packages=find_packages(),
+)
